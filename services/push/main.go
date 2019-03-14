06854922154a25a7dda7f3fc3a78079774c4e0bb
Shane Vitarana <shanev@gmail.com>
2019-03-14 00:44:23 -0700

    Sending notification for backing a story

--- services/push/main.go
+++ services/push/main.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
 	truchain "github.com/TruStory/truchain/types"
-	"github.com/TruStory/truchain/x/backing"
+	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/certificate"
 	"github.com/tendermint/tendermint/libs/pubsub/query"
 	"github.com/tendermint/tendermint/rpc/client"
 	"github.com/tendermint/tendermint/types"
 )
-
-// var msg backing.BackStoryMsg.V
 
 func main() {
 	client := client.NewHTTP("tcp://0.0.0.0:26657", "/websocket")
@@ -38,9 +38,7 @@ func main() {
 
 	for {
 		for e := range txs {
-			// fmt.Println(e)
 			pushEvent := e.(types.EventDataTx)
-			fmt.Println(pushEvent.Result.String())
 
 			var pushData truchain.PushData
 			err := json.Unmarshal(pushEvent.Result.Data, &pushData)
@@ -48,13 +46,42 @@ func main() {
 				panic(err)
 			}
 
-			fmt.Println(pushData.From.String())
+			from := pushData.From.String()
 
 			for _, tag := range pushEvent.Result.Tags {
-				fmt.Println(string(tag.Value))
-				fmt.Println(backing.BackStoryMsg.Type())
-			}
+				action := string(tag.Value)
 
+				alert := ""
+				switch action {
+				case "back_story":
+					alert = "%s backed your story"
+				case "create_challenge":
+					alert = "%s challenged your story"
+				}
+
+				if len(alert) > 0 {
+					payload := fmt.Sprintf(`{"aps":{"alert":"%s"}}`, fmt.Sprintf(alert, from))
+
+					cert, err := certificate.FromP12File("aps_cert.p12", "trust53]Jean")
+					if err != nil {
+						log.Fatal("Cert Error:", err)
+					}
+
+					notification := &apns2.Notification{}
+					notification.DeviceToken = "f329405ac61190bcb23731c39162d5e2650f4a9dea8f357b1cfbc79ddd2990b4"
+					notification.Topic = "io.trustory.alpha2"
+					notification.Payload = []byte(payload)
+
+					client := apns2.NewClient(cert).Development()
+					res, err := client.Push(notification)
+
+					if err != nil {
+						log.Fatal("Error:", err)
+					}
+
+					fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+				}
+			}
 		}
 	}
 }

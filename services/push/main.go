3aec76df4e78418bc78f980e59688942f64a94ab
Shane Vitarana <shanev@gmail.com>
2019-03-17 16:08:19 -0700

    Refactored blocking for loop to non-blocking select

--- services/push/main.go
+++ services/push/main.go
@@ -15,6 +15,51 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+func processEvent(pushEvent types.EventDataTx) {
+	var pushData truchain.PushData
+	err := json.Unmarshal(pushEvent.Result.Data, &pushData)
+	if err != nil {
+		panic(err)
+	}
+
+	from := pushData.From.String()
+
+	for _, tag := range pushEvent.Result.Tags {
+		action := string(tag.Value)
+
+		alert := ""
+		switch action {
+		case "back_story":
+			alert = "%s backed your story"
+		case "create_challenge":
+			alert = "%s challenged your story"
+		}
+
+		if len(alert) > 0 {
+			payload := fmt.Sprintf(`{"aps":{"alert":"%s"}}`, fmt.Sprintf(alert, from))
+
+			cert, err := certificate.FromP12File("aps_cert.p12", "trust53]Jean")
+			if err != nil {
+				log.Fatal("Cert Error:", err)
+			}
+
+			notification := &apns2.Notification{}
+			notification.DeviceToken = "f329405ac61190bcb23731c39162d5e2650f4a9dea8f357b1cfbc79ddd2990b4"
+			notification.Topic = "io.trustory.alpha2"
+			notification.Payload = []byte(payload)
+
+			client := apns2.NewClient(cert).Development()
+			res, err := client.Push(notification)
+
+			if err != nil {
+				log.Fatal("Error:", err)
+			}
+
+			fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+		}
+	}
+}
+
 func main() {
 	client := client.NewHTTP("tcp://0.0.0.0:26657", "/websocket")
 	err := client.Start()
@@ -30,58 +75,12 @@ func main() {
 	txs := make(chan interface{})
 	err = client.Subscribe(ctx, "trustory-push-client", query, txs)
 
-	// go func() {
-	// 	for e := range txs {
-	// 		fmt.Println("got ", e.(types.EventDataTx))
-	// 	}
-	// }()
-
-	for {
+	go func() {
 		for e := range txs {
-			pushEvent := e.(types.EventDataTx)
-
-			var pushData truchain.PushData
-			err := json.Unmarshal(pushEvent.Result.Data, &pushData)
-			if err != nil {
-				panic(err)
-			}
-
-			from := pushData.From.String()
-
-			for _, tag := range pushEvent.Result.Tags {
-				action := string(tag.Value)
-
-				alert := ""
-				switch action {
-				case "back_story":
-					alert = "%s backed your story"
-				case "create_challenge":
-					alert = "%s challenged your story"
-				}
-
-				if len(alert) > 0 {
-					payload := fmt.Sprintf(`{"aps":{"alert":"%s"}}`, fmt.Sprintf(alert, from))
-
-					cert, err := certificate.FromP12File("aps_cert.p12", "trust53]Jean")
-					if err != nil {
-						log.Fatal("Cert Error:", err)
-					}
-
-					notification := &apns2.Notification{}
-					notification.DeviceToken = "f329405ac61190bcb23731c39162d5e2650f4a9dea8f357b1cfbc79ddd2990b4"
-					notification.Topic = "io.trustory.alpha2"
-					notification.Payload = []byte(payload)
-
-					client := apns2.NewClient(cert).Development()
-					res, err := client.Push(notification)
-
-					if err != nil {
-						log.Fatal("Error:", err)
-					}
-
-					fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
-				}
-			}
+			fmt.Println(e)
+			processEvent(e.(types.EventDataTx))
 		}
-	}
+	}()
+
+	select {} // block forever
 }

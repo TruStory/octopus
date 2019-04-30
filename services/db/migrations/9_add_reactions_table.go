baa7d5e89febac710c039fc41d6efff0b8517b00
Shane Vitarana <shanev@gmail.com>
2019-04-30 13:01:43 -0700

    Added reaction table

---
+++ services/db/migrations/9_add_reactions_table.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-pg/migrations"
+)
+
+func init() {
+	migrations.MustRegisterTx(func(db migrations.DB) error {
+		fmt.Println("creating reactions table...")
+		_, err := db.Exec(`CREATE TABLE reactions(
+			id BIGSERIAL PRIMARY KEY,
+			argument_id BIGINT NOT NULL,
+			reaction_type INTEGER NOT NULL,
+			creator VARCHAR (45) NOT NULL,
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW(),
+			deleted_at TIMESTAMP
+		)`)
+		return err
+	}, func(db migrations.DB) error {
+		fmt.Println("dropping reactions table...")
+		_, err := db.Exec(`DROP TABLE reactions`)
+		return err
+	})
+}

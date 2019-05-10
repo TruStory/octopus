f198c993395b2c0acf133b5190f352683e504227
Shane Vitarana <shanev@gmail.com>
2019-05-10 14:14:54 -0700

    moved code from chain to here

---
+++ services/api/truapi/errors.go
@@ -0,0 +1,12 @@
+package truapi
+
+import "errors"
+
+// Errors for truapi module.
+var (
+	ErrFlaggedStoryEnvVarParsing = errors.New("Error parsing flagged story environment variable")
+	Err400MissingParameter       = errors.New("Missing parameter")
+	Err401NotAuthenticated       = errors.New("User not authenticated")
+	Err404ResourceNotFound       = errors.New("Resource not found")
+	Err500InternalServerError    = errors.New("Something went wrong")
+)

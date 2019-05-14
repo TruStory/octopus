00b571b4dc35b76af6cea081803161e4cb046ffb
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-05-14 19:53:50 +0530

    refactored the db tables

--- services/db/migrations/12_add_user_metrics_table.go
+++ services/db/migrations/12_add_user_metrics_table.go
@@ -13,13 +13,15 @@ func init() {
 			id BIGSERIAL PRIMARY KEY,
 			address VARCHAR (65) NOT NULL,
 			as_on_date DATE NOT NULL DEFAULT CURRENT_DATE,
+			category_id INTEGER NOT NULL,
 			total_claims VARCHAR (65) NOT NULL,
 			total_arguments VARCHAR (65) NOT NULL,
 			total_backed VARCHAR (65) NOT NULL,
 			total_challenged VARCHAR (65) NOT NULL,
 			total_given_endorsments VARCHAR (65) NOT NULL,
-			stake_earned VARCHAR (65) NOT NULL,
-			stake_lost VARCHAR (65) NOT NULL,
+			total_received_endorsments VARCHAR (65) NOT NULL,
+			total_earned VARCHAR (65) NOT NULL,
+			total_lost VARCHAR (65) NOT NULL,
 			interest_earned VARCHAR (65) NOT NULL,
 			total_amount_at_stake VARCHAR (65) NOT NULL,
 			total_amount_staked VARCHAR (65) NOT NULL,
@@ -31,7 +33,7 @@ func init() {
 		return err
 	}, func(db migrations.DB) error {
 		fmt.Println("dropping user_metrics table...")
-		_, err := db.Exec(`DROP TABLE user_stake_metrics`)
+		_, err := db.Exec(`DROP TABLE user_metrics`)
 		return err
 	})
 }

3a758012c126dd0be60329dc5efbae55ef12ec34
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-05-16 02:06:47 +0530

    calculated the bigint diff

--- services/statistia/main.go
+++ services/statistia/main.go
@@ -11,32 +11,58 @@ import (
 
 func main() {
 
-	yesterday := time.Now().Format("2006-01-02")
+	yesterday := time.Now().Add(-24 * 5 * time.Hour).Format("2006-01-02")
 	today := time.Now().Format("2006-01-02")
+	fmt.Printf("%v -- %v\n\n", yesterday, today)
 
 	yesterdayMetrics := fetchMetrics(yesterday)
 	todayMetrics := fetchMetrics(today)
 	for address, todayMetric := range todayMetrics.Users {
-		yesterdayMetric, ok := yesterdayMetrics.Users[address]
-		if ok {
-			perDayMetric := &UserMetrics{
-				TotalClaims: big.NewInt(0).Sub(
-					big.NewInt(todayMetric.TotalClaims), big.NewInt(yesterdayMetric.TotalClaims),
-				).Int64(),
-				TotalArguments: big.NewInt(0).Sub(
-					big.NewInt(todayMetric.TotalArguments), big.NewInt(yesterdayMetric.TotalArguments),
-				).Int64(),
-				TotalGivenEndorsements: big.NewInt(0).Sub(
-					big.NewInt(todayMetric.TotalGivenEndorsements), big.NewInt(yesterdayMetric.TotalGivenEndorsements),
-				).Int64(),
-			}
+		for categoryID, todayCategoryMetric := range todayMetric.CategoryMetrics {
+			yesterdayMetric, ok := yesterdayMetrics.Users[address]
+			if ok {
+				yesterdayCategoryMetric, ok := yesterdayMetric.CategoryMetrics[categoryID]
+				if ok {
+					fmt.Printf("\n\n%v\n%v", todayCategoryMetric, yesterdayCategoryMetric)
+					dailyCategoryMetric := &Metrics{
+						TotalClaims:               todayCategoryMetric.Metrics.TotalClaims - yesterdayCategoryMetric.Metrics.TotalClaims,
+						TotalArguments:            todayCategoryMetric.Metrics.TotalArguments - yesterdayCategoryMetric.Metrics.TotalArguments,
+						TotalGivenEndorsements:    todayCategoryMetric.Metrics.TotalGivenEndorsements - yesterdayCategoryMetric.Metrics.TotalGivenEndorsements,
+						TotalReceivedEndorsements: todayCategoryMetric.Metrics.TotalReceivedEndorsements - yesterdayCategoryMetric.Metrics.TotalReceivedEndorsements,
+						TotalAmountStaked:         subtractCoin(todayCategoryMetric.Metrics.TotalAmountStaked, yesterdayCategoryMetric.Metrics.TotalAmountStaked),
+						TotalAmountAtStake:        subtractCoin(todayCategoryMetric.Metrics.TotalAmountAtStake, yesterdayCategoryMetric.Metrics.TotalAmountAtStake),
+						StakeEarned:               subtractCoin(todayCategoryMetric.Metrics.StakeEarned, yesterdayCategoryMetric.Metrics.StakeEarned),
+						StakeLost:                 subtractCoin(todayCategoryMetric.Metrics.StakeLost, yesterdayCategoryMetric.Metrics.StakeLost),
+						InterestEarned:            subtractCoin(todayCategoryMetric.Metrics.InterestEarned, yesterdayCategoryMetric.Metrics.InterestEarned),
+					}
 
-			fmt.Printf("\nUser -- %v\nMetrics -- %v\nPer Day -- %v\n\n", address, todayMetric, perDayMetric)
+					fmt.Printf("\nUser -- %v\nMetrics -- %v\nPer Day -- %v\n\n", address, todayMetric, dailyCategoryMetric)
+				}
+
+			}
 		}
 	}
 }
 
-func fetchMetrics(date string) *Metrics {
+func subtractCoin(coinA, coinB Coin) Coin {
+	if coinA.Denom != coinB.Denom {
+		panic("denom must be same when calculating diff")
+	}
+
+	bigIntCoinA, _ := new(big.Int).SetString(coinA.Amount, 10)
+	bigIntCoinB, _ := new(big.Int).SetString(coinB.Amount, 10)
+
+	fmt.Printf("\nBIG INT DIFF -- %v -- %v\n", bigIntCoinA, bigIntCoinB)
+
+	bigIntDiff := new(big.Int).Sub(bigIntCoinA, bigIntCoinB)
+
+	return Coin{
+		Denom:  coinA.Denom,
+		Amount: bigIntDiff.String(),
+	}
+}
+
+func fetchMetrics(date string) *MetricsSummary {
 	client := &http.Client{}
 	request, err := http.NewRequest("GET", "http://localhost:1337/api/v1/metrics?date="+date, nil)
 	if err != nil {
@@ -55,7 +81,7 @@ func fetchMetrics(date string) *Metrics {
 		panic(err)
 	}
 
-	metrics := &Metrics{
+	metrics := &MetricsSummary{
 		Users: make(map[string]*UserMetrics),
 	}
 	err = json.Unmarshal(body, &metrics)
--- services/statistia/types.go
+++ services/statistia/types.go
@@ -36,35 +36,60 @@ func (b *BigInt) UnmarshalJSON(p string) error {
 // Coin hold some amount of one currency.
 type Coin struct {
 	Denom  string `json:"denom"`
-	Amount BigInt `json:"amount"`
+	Amount string `json:"amount"`
 }
 
 // Minus subtracts amounts of two coins with same denom. If the coins differ in denom
 // then it panics.
-func (coin Coin) Minus(coinB Coin) Coin {
-	res := Coin{coin.Denom, coin.Amount.Sub(coinB.Amount)}
-	if res.IsNegative() {
-		panic("negative count amount")
-	}
+// func (coin Coin) Minus(coinB Coin) Coin {
+// 	res := Coin{coin.Denom, coin.Amount.Sub(coinB.Amount)}
+// 	if res.IsNegative() {
+// 		panic("negative count amount")
+// 	}
 
-	return res
+// 	return res
+// }
+
+// MetricsSummary represents metrics for the platform.
+type MetricsSummary struct {
+	Users map[string]*UserMetrics `json:"users"`
 }
 
-// Metrics represents metrics for the platform.
+// AccumulatedUserCred tracks accumulated cred by day.
+type AccumulatedUserCred map[string]Coin
+
+// Metrics tracked.
 type Metrics struct {
-	Users map[string]*UserMetrics `json:"users"`
+	// Interactions
+	TotalClaims               int64 `json:"total_claims"`
+	TotalArguments            int64 `json:"total_arguments"`
+	TotalReceivedEndorsements int64 `json:"total_received_endorsements"`
+	TotalGivenEndorsements    int64 `json:"total_given_endorsments"`
+
+	// StakeBased Metrics
+	TotalAmountStaked  Coin `json:"total_amount_staked"`
+	StakeEarned        Coin `json:"stake_earned"`
+	StakeLost          Coin `json:"stake_lost"`
+	TotalAmountAtStake Coin `json:"total_amount_at_stake"`
+	InterestEarned     Coin `json:"interest_earned"`
+}
+
+// CategoryMetrics summary of metrics by category.
+type CategoryMetrics struct {
+	CategoryID   int64    `json:"category_id"`
+	CategoryName string   `json:"category_name"`
+	CredBalance  Coin     `json:"cred_balance"`
+	Metrics      *Metrics `json:"metrics"`
 }
 
 // UserMetrics a summary of different metrics per user
 type UserMetrics struct {
-	TotalClaims            int64 `json:"total_claims"`
-	TotalArguments         int64 `json:"total_arguments"`
-	TotalGivenEndorsements int64 `json:"total_given_endorsments"`
+	UserName string `json:"username"`
+	Balance  Coin   `json:"balance"`
+
+	// ByCategoryID
+	CategoryMetrics map[int64]*CategoryMetrics `json:"category_metrics"`
+
 	// Tracked by day
-	// CredEarned         map[string]AccumulatedUserCred
-	InterestEarned     Coin `json:"intereset_earned"`
-	StakeLost          Coin `json:"stake_lost"`
-	StakeEarned        Coin `json:"stake_earned"`
-	TotalAmountAtStake Coin `json:"total_amount_at_stake"`
-	TotalAmountStaked  Coin `json:"total_amount_staked"`
+	CredEarned map[string]AccumulatedUserCred
 }

2873c2c9426eb5e4fe2f5ecc964a77076d9879db
Ted Piotrowski <ted@trustory.io>
2019-05-28 15:01:27 -0700

    Ignore error when cookie not present

--- services/truapi/truapi/truapi.go
+++ services/truapi/truapi/truapi.go
@@ -130,7 +130,7 @@ func (ta *TruAPI) RegisterRoutes(apiCtx truCtx.TruAPIContext) {
 	ta.PathPrefix("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// if it is not requesting a file with a valid extension serve the index
 		if filepath.Ext(path.Base(r.URL.Path)) == "" {
-			webVersionCookie, err := r.Cookie("web_version")
+			webVersionCookie, _ := r.Cookie("web_version")
 			webDirectory := apiCtx.Config.Web.Directory;
 			if webVersionCookie != nil && webVersionCookie.Value == "2" {
 				webDirectory = apiCtx.Config.Web.DirectoryV2;

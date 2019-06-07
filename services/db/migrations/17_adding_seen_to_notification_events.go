fd414d56972e4fc8c300198d6ea73d0b96a3e09e
Ted Piotrowski <ted@trustory.io>
2019-06-07 09:58:46 -0700

    Update migration to correctly set default value of seen notifications

--- services/db/migrations/17_adding_seen_to_notification_events.go
+++ services/db/migrations/17_adding_seen_to_notification_events.go
@@ -10,6 +10,10 @@ func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		fmt.Println("adding seen column to notification_events...")
 		_, err := db.Exec(`ALTER TABLE notification_events ADD COLUMN seen BOOLEAN, ALTER COLUMN seen SET DEFAULT FALSE `)
+		if err != nil {
+			return err
+		}
+		_, err = db.Exec(`UPDATE notification_events SET seen = read`)
 		return err
 	}, func(db migrations.DB) error {
 		fmt.Println("removing seen column from notification_events...")

4dba91599c9ad9b793a23ea5c9faaa90bea82fca
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-07-12 10:54:05 +0530

    using username instead of real names

--- services/spotlight/main.go
+++ services/spotlight/main.go
@@ -184,7 +184,7 @@ func compileClaimPreview(raw []byte, claim ClaimObject) string {
 	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__ARGUMENT_COUNT"), []byte(strconv.Itoa(claim.ArgumentCount)), -1)
 
 	// CREATED BY
-	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(claim.Creator.TwitterProfile.FullName), -1)
+	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(claim.Creator.TwitterProfile.Username), -1)
 
 	// SOURCE
 	if claim.HasSource() {
@@ -215,7 +215,7 @@ func compileArgumentPreview(raw []byte, argument ArgumentObject) string {
 	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__AGREE_COUNT"), []byte(strconv.Itoa(argument.UpvotedCount)), -1)
 
 	// CREATED BY
-	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(argument.Creator.TwitterProfile.FullName), -1)
+	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(argument.Creator.TwitterProfile.Username), -1)
 
 	return string(compiled)
 }
@@ -236,7 +236,7 @@ func compileCommentPreview(raw []byte, comment CommentObject) string {
 	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__BODY_LINE_3"), []byte(bodyLines[2]), -1)
 
 	// CREATED BY
-	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(comment.Creator.TwitterProfile.FullName), -1)
+	compiled = bytes.Replace(compiled, []byte("$PLACEHOLDER__CREATOR"), []byte(comment.Creator.TwitterProfile.Username), -1)
 
 	return string(compiled)
 }

902970c8029b31b3c0d588d5925b9e09c625ba29
jhernandezb <contact@jhernandez.me>
2019-07-12 18:57:28 -0600

    Fixed migrations for different pg version

--- services/db/migrations/24_migrate_track_events.go
+++ services/db/migrations/24_migrate_track_events.go
@@ -9,31 +9,31 @@ import (
 func init() {
 	migrations.MustRegisterTx(func(db migrations.DB) error {
 		fmt.Println("updating track_events")
-		_, err := db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"crypto"}') WHERE (meta->'categoryId')::INTEGER = 1`)
+		_, err := db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"crypto"}') WHERE (meta->>'categoryId')::INTEGER = 1`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"trustory"}') WHERE (meta->'categoryId')::INTEGER = 2`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"trustory"}') WHERE (meta->>'categoryId')::INTEGER = 2`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"general"}') WHERE (meta->'categoryId')::INTEGER = 3`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"general"}') WHERE (meta->>'categoryId')::INTEGER = 3`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"cosmos"}') WHERE (meta->'categoryId')::INTEGER = 4`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"cosmos"}') WHERE (meta->>'categoryId')::INTEGER = 4`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"entertainment"}') WHERE (meta->'categoryId')::INTEGER = 5`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"entertainment"}') WHERE (meta->>'categoryId')::INTEGER = 5`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"tech"}') WHERE (meta->'categoryId')::INTEGER = 6`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"tech"}') WHERE (meta->>'categoryId')::INTEGER = 6`)
 		if err != nil {
 			return err
 		}
-		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"sports"}') WHERE (meta->'categoryId')::INTEGER = 7`)
+		_, err = db.Exec(`UPDATE track_events SET meta = (meta - 'categoryId'  || '{"communityId":"sports"}') WHERE (meta->>'categoryId')::INTEGER = 7`)
 		return err
 	}, func(db migrations.DB) error {
 		return nil

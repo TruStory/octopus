10ff3e302cbce96ed41c363eb0a618a175708237
Ted Piotrowski <ted@trustory.io>
2019-07-19 10:07:27 -0700

    Allow all claim admins to edit claim image

--- services/truapi/truapi/handle_claim_image.go
+++ services/truapi/truapi/handle_claim_image.go
@@ -38,9 +38,10 @@ func (ta *TruAPI) updateClaimImage(r *http.Request) chttp.Response {
 	}
 
 	claim := ta.claimResolver(r.Context(), queryByClaimID{ID: request.ClaimID})
+	settings := ta.settingsResolver(r.Context())
 
 	// preethis cosmos address for admin privilieges
-	if claim.Creator.String() != user.Address && user.Address != "cosmos1xqc5gwzpg3fyv5en2fzyx36z2se5ks33tt57e7" {
+	if claim.Creator.String() != user.Address && contains(settings.ClaimAdmins, user.Address) {
 		return chttp.SimpleErrorResponse(403, Err403NotAuthorized)
 	}
 

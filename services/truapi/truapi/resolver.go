0f40fe05c122d00b74b36f5095f76b12f23f529c
Aamir Latif <aamir@trustory.io>
2019-08-27 15:09:10 -0700

    Removed panic

--- services/truapi/truapi/resolver.go
+++ services/truapi/truapi/resolver.go
@@ -740,7 +740,7 @@ func (ta *TruAPI) appAccountSlashResolver(ctx context.Context, q staking.Argumen
 func (ta *TruAPI) claimCommentsResolver(ctx context.Context, q queryByClaimID) []db.Comment {
 	comments, err := ta.DBClient.CommentsByClaimID(q.ID)
 	if err != nil {
-		panic(err)
+		fmt.Println("claimComments err: ", err)
 	}
 	return comments
 }
@@ -748,7 +748,7 @@ func (ta *TruAPI) claimCommentsResolver(ctx context.Context, q queryByClaimID) [
 func (ta *TruAPI) claimQuestionsResolver(ctx context.Context, q queryByClaimID) []db.Question {
 	questions, err := ta.DBClient.QuestionsByClaimID(q.ID)
 	if err != nil {
-		panic(err)
+		fmt.Println("claimQuestions err: ", err)
 	}
 	return questions
 }

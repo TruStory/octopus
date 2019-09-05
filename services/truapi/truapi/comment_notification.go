9d171b4f33c00df85c063309ad6ef621501d30bf
Ted Piotrowski <ted@trustory.io>
2019-09-05 10:42:12 -0700

    Use dataloadercontext for notifications

--- services/truapi/truapi/comment_notification.go
+++ services/truapi/truapi/comment_notification.go
@@ -20,14 +20,14 @@ func (ta *TruAPI) runCommentNotificationSender(notifications <-chan CommentNotif
 	url := fmt.Sprintf("%s/%s", strings.TrimRight(strings.TrimSpace(endpoint), "/"), "sendCommentNotification")
 
 	for n := range notifications {
-		claim := ta.claimResolver(context.Background(), queryByClaimID{ID: uint64(n.ClaimID)})
+		claim := ta.claimResolver(ta.createContext(context.Background()), queryByClaimID{ID: uint64(n.ClaimID)})
 		if claim.ID == 0 {
 			fmt.Println("error retrieving claim id", n.ClaimID)
 			continue
 		}
 		n.ClaimCreator = claim.Creator.String()
 		if n.ArgumentID != 0 {
-			argument := ta.claimArgumentResolver(context.Background(), queryByArgumentID{ID: uint64(n.ArgumentID)})
+			argument := ta.claimArgumentResolver(ta.createContext(context.Background()), queryByArgumentID{ID: uint64(n.ArgumentID)})
 			if argument.ID == 0 {
 				fmt.Println("error retrieving argument id", n.ArgumentID)
 				continue

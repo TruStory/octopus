03aae36483b32aefd84da80d4b462445d7293edf
jhernandezb <contact@jhernandez.me>
2019-10-24 14:57:57 -0600

    Add domain whitelist for registration

---
+++ services/db/migrations/53_add_domain_whitelist.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-pg/migrations"
+)
+
+func init() {
+	migrations.MustRegisterTx(func(db migrations.DB) error {
+		fmt.Println("adding domain_whitelist table...")
+
+		_, err := db.Exec(`CREATE TABLE domain_whitelists (
+			id BIGSERIAL PRIMARY KEY,
+			domain TEXT NOT NULL UNIQUE,
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW(),
+			deleted_at TIMESTAMP
+		)`)
+		return err
+	}, func(db migrations.DB) error {
+		fmt.Println("dropping domain_whitelists table...")
+		_, err := db.Exec(`DROP TABLE domain_whitelist`)
+		return err
+	})
+}

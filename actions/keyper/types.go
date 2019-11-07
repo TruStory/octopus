503840ac28f0bad0a30f2538b11030044330098e
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-11-07 21:39:03 +0530

    remove unused methods

--- actions/keyper/types.go
+++ actions/keyper/types.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
-
-func mustEnv(env string) string {
-	val := os.Getenv(env)
-	if val == "" {
-		panic(fmt.Sprintf("must provide %s variable", env))
-	}
-	return val
-}
 
 func getEnv(env, defaultValue string) string {
 	val := os.Getenv(env)

d2bb869d390d7d37667d0ad15544851c7fd1c10b
Mohit Mamoria <mohit.mamoria@gmail.com>
2019-11-14 13:39:40 +0530

    added the CMD flag to run snowball for selective users

--- services/truapi/db/user.go
+++ services/truapi/db/user.go
@@ -775,6 +775,19 @@ func (c *Client) UsersByAddress(addresses []string) ([]User, error) {
 	return users, nil
 }
 
+// UsersByID fetches a list of users by id
+func (c *Client) UsersByID(ids []int64) ([]User, error) {
+	users := make([]User, 0)
+	err := c.Model(&users).WhereIn("id in (?)", pg.In(ids)).Select()
+	if err == pg.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
 // UserProfileByUsername fetches user profile by username
 func (c *Client) UserProfileByUsername(username string) (*UserProfile, error) {
 	userProfile := new(UserProfile)
